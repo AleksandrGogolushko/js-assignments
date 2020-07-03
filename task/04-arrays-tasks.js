ec9b0e4bfa79eff09e347d55f1952d790388a92a
AleksandrGogolushko <gogolushko@outlook.com>
2020-07-03 12:16:57 +0300

    implement selectMany

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -563,8 +563,8 @@ function group(array, keySelector, valueSelector) {
  *   ['one','two','three'], x=>x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
 function selectMany(arr, childrenSelector) {
-   throw new Error('Not implemented');
-}
+   return arr.map(e => childrenSelector(e)).flat()
+ }
 
 
 /**
