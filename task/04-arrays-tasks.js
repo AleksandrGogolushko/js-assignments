04102607d1b1c26609e3b647f60749a431963627
AleksandrGogolushko <gogolushko@outlook.com>
2020-07-03 12:21:04 +0300

    selectMany

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -563,8 +563,8 @@ function group(array, keySelector, valueSelector) {
  *   ['one','two','three'], x=>x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
 function selectMany(arr, childrenSelector) {
-   return arr.map(e => childrenSelector(e)).reduce((a,b)=> a.concat(b))
- }
+   return arr.map(e => childrenSelector(e)).reduce((a, b) => a.concat(b))
+}
 
 
 /**
